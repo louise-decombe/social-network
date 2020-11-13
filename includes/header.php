308f3fd79cb400e56e71b8ba2d1725ee76a7621a
louise-decombe <louise.decombe@laplateforme.io>
2020-11-13 09:47:07 +0100

    header navbar

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,31 @@
 
+      <!---------------------------------------------------------------------------------------------------------- 
+      Se trouvent dans cette page, la navbar et tout les liens de script / css nécessaires dans le head + les meta. 
+    ---------------------------------------------------------------------------------------------------------------->
+
 
 <?php  ?>
 
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
-
-
-
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- liens css (bootstrap, fontawesome, css) -->
+      <link rel="stylesheet" href="css/styles.css">
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+      <!-- polices caractère -->
+      <link href="" rel="stylesheet">
+      <!-- liens script, jquery ajax  -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 </head>
 
-    
-</body>
-</html>
-
 <header>
-
+<!--  Include de la navbar -->
 <?php include('navbar.php') ?>
     
 </header>
