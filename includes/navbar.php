d64a4089df63760983a0e4f5fdab5ab2447a49b4
louise-decombe <louise.decombe@laplateforme.io>
2020-11-13 11:02:21 +0100

    maj liens navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,7 +9,7 @@
 
  ?>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-   <a class="navbar-brand" href="#">
+   <a class="navbar-brand" href="index.php">
   </a>
   
   <ul class="navbar-nav">
@@ -20,7 +20,7 @@
       <img src="images/default-profile.png" alt="Logo" style="width:40px;">
       </a>
       <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Profil</a>
+        <a class="dropdown-item" href="profile.php">Profil</a>
         <a class="dropdown-item" href="#">Paramètres</a>
         <div class="dropdown-divider"></div>
                      <form action="index.php" method="post">
@@ -28,55 +28,39 @@
                      </form>      </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#"> <button class="btn btn-primary"> Messages <i class="far fa-comments" width=""></i></button>
+      <a class="nav-link" href="chat.php"> <button class="btn btn-primary"> Messages <i class="far fa-comments" width=""></i></button>
 
 </a>
     </li>
     
     <li class="nav-item">
-    <a class="nav-link" href="#"> <button class="btn btn-primary">   MUR</button> </a>
+    <a class="nav-link" href="index.php"> <button class="btn btn-primary">Mur <i class="fa fa-home" aria-hidden="true"></i></button> </a>
     </li>
-    <form class="form-inline my-2 my-lg-0" action="/action_page.php">
-    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-    <button class="btn btn-primary" type="submit">Chercher</button>
-  </form>
+
+    <li class="nav-item">
+					<input type="text" placeholder="Chercher un utilisateur"  class= "search form-control mr-sm-2"/>
+					<div class="search-result">
+					</div>
+				</li>
   </ul>
 </nav>
 
 <?php //}else{ ?>
 
-  <!--  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-</div 
+  <!--   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <a class="navbar-brand" href="index.php">
+  </a>
+  
+  <ul class="navbar-nav">
+    
+    <li class="nav-item">
+    <a class="nav-link" href="connexion.php"> Connexion </button> </a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="inscription.php"> Inscription </button> </a>
+    </li>
+  </ul>
+</nav>
     
 <? //} ?> 
 
