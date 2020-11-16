00950de51da97cfcb4b7be7d536553f2226e98ef
louise-decombe <louise.decombe@laplateforme.io>
2020-11-16 09:24:25 +0100

    librairie emoticon pour chat / test

--- chat.php
+++ chat.php
@@ -8,9 +8,25 @@ session_start();
     include("includes/header.php");
    ?>
 
+
 <main>
 
+<div class="container">
 
+<textarea name="" id="mytext"></textarea>
+
+
+</div>
 
 </main>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.2/emojionearea.min.js"></script>
+<script>
+
+$("#mytext").emojioneArea({
+
+    pickerPosition:"right"
+
+});
+
+</script>
 <?php include('includes/footer.php') ?>
--- includes/header.php
+++ includes/header.php
@@ -16,12 +16,12 @@
       <link rel="stylesheet" href="css/style.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.2/emojionearea.css">
       <!-- polices caractère -->
       <link href="" rel="stylesheet">
       <!-- liens script, jquery ajax  -->
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="js/search.js"></script>
     </head>
 
