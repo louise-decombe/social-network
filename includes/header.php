75c855e863093a22d00a97088e74e62784d3709c
nadia-rais <nadia.rais@laplateforme.io>
2020-11-16 11:03:51 +0100

    démarrage partie user

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,12 @@
-<?php include('../class/Config.php'); ?>
+<?php include('class/Config.php'); ?>
 
 <header>
-    
-
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+            Plateformer_
+        </a>
+    </nav>
 </header>
 
 
