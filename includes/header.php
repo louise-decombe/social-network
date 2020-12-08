8711d327adbcd647926931df012dffc6b403a7f2
louise <louise.decombe@laplateforme.io>
2020-12-08 10:44:05 +0100

    modif url

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,7 @@ $_SESSION['id'] = 1;
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- liens css (bootstrap, fontawesome, css) -->
       <link rel="stylesheet" href="css/style.css">
-      <link rel="stylesheet" href="css/style-complete.css">
+      <link rel="stylesheet" href="css/chat.css">
 
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
