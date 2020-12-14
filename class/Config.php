e680e70e7c6e63b31c165ea3d52fbb1bbbc6d615
louise-decombe <louise.decombe@laplateforme.io>
2020-12-14 11:10:27 +0100

    mise à jour config avec toutes les classes ( test_user 1 en commentaire)

--- class/Config.php
+++ class/Config.php
@@ -1,18 +1,30 @@
 <?php 
 
-    require 'Db.php';
-<<<<<<< HEAD
-    require 'User.php';
-    $db = new DB();
-    $user = new User($db);
-=======
-    $db = new DB();
 
-    require 'test_user.php';
+require 'Db.php';
+$db = new DB();
+    
+    require 'User.php';
     $user = new User($db);
+
+    //require 'test_user.php';
+    //$user = new User($db);
 
     require 'Post.php';
     $post = new Post($db);
->>>>>>> celine
+
+    require 'Search.php';
+    $search = new Search($db);
+
+    require 'Message.php';
+    $message = new Message($db);
+
+    require 'Hashtag.php';
+    $tendance = new Hashtag($db);
+
+    //traitement de la déconnexion
+if (isset($_POST["deco"])) {
+    $user->disconnect();
+}
     
 ?>
