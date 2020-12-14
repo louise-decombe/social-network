3908a0aa2650511165e40bf59197fff9bf47eb6f
louise-decombe <louise.decombe@laplateforme.io>
2020-12-14 11:11:50 +0100

    ajout class Louise + client chat mis à jour

---
+++ class/Search.php
@@ -0,0 +1,42 @@
+<?php
+Class Search  {
+
+    private $db;
+
+    public function __construct($db){
+        $this->db = $db;
+        $this->connect = $this->db->connectDb();
+    }
+
+
+public function checkInput($data){
+    $data = htmlspecialchars($data);
+    $data = trim($data);
+    $data = stripcslashes($data);
+    return $data;
+}
+public function search($search){
+    $stmt = $this->connect->prepare("SELECT `id`,`firstname`,`lastname`,`photo` FROM `users` WHERE `firstname` LIKE ? OR `lastname` LIKE ?");
+    $stmt->bindValue(1, $search.'%', PDO::PARAM_STR);
+    $stmt->bindValue(2, $search.'%', PDO::PARAM_STR);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
+}
+
+public function create($table, $fields = array()){
+    $columns = implode(',', array_keys($fields));
+    $values  = ':'.implode(', :', array_keys($fields));
+    $sql     = "INSERT INTO {$table} ({$columns}) VALUES ({$values})";
+
+    if($stmt = $this->connect->prepare($sql)){
+        foreach ($fields as $key => $data) {
+            $stmt->bindValue(':'.$key, $data);
+        }
+        $stmt->execute();
+        return $this->connect->lastInsertId();
+    }
+}
+}
+
+
+?>
