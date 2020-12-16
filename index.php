8406f8efa0f7af958e8774fb1b0c02ffc984513c
nadia-rais <nadia.rais@laplateforme.io>
2020-12-16 11:00:41 +0100

    update fichiers

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ session_start();
     <link rel="stylesheet" type="text/css" href="css/style-forms.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></script>-->
 </head>
 <body>
 <header>
