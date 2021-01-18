3e348ceb0566e53f7f5f9d24a534bce85a330115
Louise Décombe <62133595+louise-decombe@users.noreply.github.com>
2021-01-18 11:02:04 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,10 +6,7 @@
 if(isset($_SESSION['user']['id'])){
 ?>
 
- 
-<head>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</head>
 <header>
    <!--  Include de la navbar -->
    <?php 
