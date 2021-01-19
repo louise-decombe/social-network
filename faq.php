ddba63be111371bde142631d1211ccd17a624f9f
louise <louise.decombe@laplateforme.io>
2021-01-19 19:21:45 +0100

    ajout faq

---
+++ faq.php
@@ -0,0 +1,148 @@
+<?php
+    include 'class/Config.php';
+    ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- liens css (css) -->
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/hashtag.css">
+      <link  rel="stylesheet" href="css/styles.css">
+      <link rel="stylesheet" type="text/css" href="css/fontello/css/fontello.css">
+      <link rel="stylesheet" href="css/faq.css">
+
+      <link rel="stylesheet" href="css/chat.css">
+      <link rel="stylesheet" type="text/css" href="css/style-forms.css">
+     <!-- liens css (bootstrap, fontawesome) -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <link rel="shortcut icon" type="image/x-icon" href="#">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <!-- liens script, jquery ajax -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <!--    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ -->
+    <script src="js/search.js"></script>
+    <script src="js/chat.js"></script>
+    <script src="js/search_chat.js"></script>
+    <script src="js/envoi_message.js"></script>
+     <script src="js/hashtag.js"></script>
+    </head>
+  <header>
+
+    <?php include('includes/header.php');?>
+   
+</header>
+
+<body>
+
+<main>
+
+
+<main>
+  	<center><h2> Les Plateformer - Terms and conditions
+</h2></center>
+
+<div class="container">
+  <ul class="acc">
+
+  <li>
+    <button class="acc_ctrl"><h2> Article 1 - Objet</h2></button>
+    <div class="acc_panel">
+<p>
+Dapibus, ultricies ullamcorper egestas. Dictum mattis condimentum platea convallis tellus a. Dis vehicula habitant phasellus enim fringilla rhoncus, rhoncus ultricies sagittis class hendrerit ipsum. Orci id integer inceptos volutpat at consequat litora odio blandit. Mi interdum laoreet sollicitudin lorem ridiculus volutpat posuere metus montes. Elit amet cras, quis fermentum. Inceptos nunc justo ad quis auctor torquent ut, arcu natoque augue maecenas ad! Habitasse sociosqu luctus, natoque himenaeos neque arcu tempus ultricies orci interdum rhoncus. Bibendum proin posuere massa sociosqu.
+</p>
+    </div>
+  </li>
+
+    <li>
+      <button class="acc_ctrl"><h2>Article 2- Disponibilité</h2></button>
+      <div class="acc_panel">
+        <p>  Nos produits sont proposés tant qu'ils sont visibles sur le site ......... et dans la limite des stocks disponibles. Pour les produits non stockés, nos offres sont valables sous réserve de disponibilité chez nos fournisseurs.
+          En cas d'indisponibilité de produit après passation de votre commande, nous vous en informerons par mail. Votre commande sera automatiquement annulée et aucun débit bancaire ne sera effectué.
+
+          En outre, le site Internet ......... n'a pas vocation à vendre ses produits en quantités importantes. Par conséquent la sociétéHIGH & CRAFT FANZINE BOOKSTORE se réserve le droit de refuser les commandes de ......... (nombre) articles identiques.
+        </p>
+      </div>
+    </li>
+
+
+
+
+      <li>
+        <button class="acc_ctrl"><h2>Article 3 - Responsabilité</h2></button>
+        <div class="acc_panel">
+          <p>Dapibus, ultricies ullamcorper egestas. Dictum mattis condimentum platea convallis tellus a. Dis vehicula habitant phasellus enim fringilla rhoncus, rhoncus ultricies sagittis class hendrerit ipsum. Orci id integer inceptos volutpat at consequat litora odio blandit. Mi interdum laoreet sollicitudin lorem ridiculus volutpat posuere metus montes. Elit amet cras, quis fermentum. Inceptos nunc justo ad quis auctor torquent ut, arcu natoque augue maecenas ad! Habitasse sociosqu luctus, natoque himenaeos neque arcu tempus ultricies orci interdum rhoncus. Bibendum proin posuere massa sociosqu.
+ </p>
+        </div>
+      </li>
+
+      <li>
+        <button class="acc_ctrl"><h2>Article 4 - Droit applicable en cas de litiges</h2></button>
+        <div class="acc_panel">
+          <p> Dapibus, ultricies ullamcorper egestas. Dictum mattis condimentum platea convallis tellus a. Dis vehicula habitant phasellus enim fringilla rhoncus, rhoncus ultricies sagittis class hendrerit ipsum. Orci id integer inceptos volutpat at consequat litora odio blandit. Mi interdum laoreet sollicitudin lorem ridiculus volutpat posuere metus montes. Elit amet cras, quis fermentum. Inceptos nunc justo ad quis auctor torquent ut, arcu natoque augue maecenas ad! Habitasse sociosqu luctus, natoque himenaeos neque arcu tempus ultricies orci interdum rhoncus. Bibendum proin posuere massa sociosqu.
+  </p>
+        </div>
+      </li>
+
+      <li>
+        <button class="acc_ctrl"><h2>Article 5- Propriété intellectuelle</h2></button>
+        <div class="acc_panel">
+          <p> Dapibus, ultricies ullamcorper egestas. Dictum mattis condimentum platea convallis tellus a. Dis vehicula habitant phasellus enim fringilla rhoncus, rhoncus ultricies sagittis class hendrerit ipsum. Orci id integer inceptos volutpat at consequat litora odio blandit. Mi interdum laoreet sollicitudin lorem ridiculus volutpat posuere metus montes. Elit amet cras, quis fermentum. Inceptos nunc justo ad quis auctor torquent ut, arcu natoque augue maecenas ad! Habitasse sociosqu luctus, natoque himenaeos neque arcu tempus ultricies orci interdum rhoncus. Bibendum proin posuere massa sociosqu.
+  </p>
+        </div>
+      </li>
+
+
+      <li>
+        <button class="acc_ctrl"><h2>Article 6 - Données personnelles</h2></button>
+        <div class="acc_panel">
+          <p> Dapibus, ultricies ullamcorper egestas. Dictum mattis condimentum platea convallis tellus a. Dis vehicula habitant phasellus enim fringilla rhoncus, rhoncus ultricies sagittis class hendrerit ipsum. Orci id integer inceptos volutpat at consequat litora odio blandit. Mi interdum laoreet sollicitudin lorem ridiculus volutpat posuere metus montes. Elit amet cras, quis fermentum. Inceptos nunc justo ad quis auctor torquent ut, arcu natoque augue maecenas ad! Habitasse sociosqu luctus, natoque himenaeos neque arcu tempus ultricies orci interdum rhoncus. Bibendum proin posuere massa sociosqu.
+ </p>
+        </div>
+      </li>
+
+
+      <li>
+        <button class="acc_ctrl"><h2>Article 7   - Archivage Preuve</h2></button>
+        <div class="acc_panel">
+          <p> Dapibus, ultricies ullamcorper egestas. Dictum mattis condimentum platea convallis tellus a. Dis vehicula habitant phasellus enim fringilla rhoncus, rhoncus ultricies sagittis class hendrerit ipsum. Orci id integer inceptos volutpat at consequat litora odio blandit. Mi interdum laoreet sollicitudin lorem ridiculus volutpat posuere metus montes. Elit amet cras, quis fermentum. Inceptos nunc justo ad quis auctor torquent ut, arcu natoque augue maecenas ad! Habitasse sociosqu luctus, natoque himenaeos neque arcu tempus ultricies orci interdum rhoncus. Bibendum proin posuere massa sociosqu.
+ </p>
+        </div>
+      </li>
+
+</ul>
+</div>
+    </main>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() {
+$('.acc_ctrl').on('click', function(e) {
+e.preventDefault();
+if ($(this).hasClass('active')) {
+$(this).removeClass('active');
+$(this).next()
+.stop()
+.slideUp(300);
+} else {
+$(this).addClass('active');
+$(this).next()
+.stop()
+.slideDown(300);
+}
+});
+});
+</script>
+
+
+</body>
+
+
+<footer>
+    <?php
+    include("includes/footer.php") ?>
+</footer>
